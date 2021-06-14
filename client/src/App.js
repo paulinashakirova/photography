afa1589a9db59fd43016aaea622ec0c0c48f6d40
karenina-pecas <arq.karengallegos89@gmail.com>
2021-06-14 15:59:48 +0200

    full stack app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Karenina Photography</h1>
+      </header>
+    </div>
+  );
+}
+
+
