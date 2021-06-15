f4c7aaf1fef2b4647c4282d710426202309c4336
karenina-pecas <arq.karengallegos89@gmail.com>
2021-06-15 14:24:49 +0200

    Working on endpoints

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+var photographyRouter = require('./routes/photography');
+
 
 var app = express();
 
@@ -16,8 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/photography', photographyRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/photography.js
+++ routes/photography.js
@@ -2,23 +2,35 @@ var express = require('express');
 var router = express.Router();
 const db = require("../model/helper");
 
-/* GET photography listing. */
+/* GET topic listing. */
 router.get('/', async (req, res, next) => {
   try {
-    const results = await db("SELECT * FROM topics;");
+    const results = await db("SELECT * FROM topic;");
     res.send(results.data);
   } catch (err) {
     res.status(500).send(err);
   }  
 });
 
-// GET one theme
-router.get("/:theme_id", async (req, res, next) => {
+// GET one topic
+router.get("/:topic_id", async (req, res, next) => {
   try {
     const results = await db(
-      `select theme, description from topics where theme_id = ${req.params.theme_id};`
+      `select topic_id, theme, description, image from topic where topic_id = ${req.params.topic_id};`
     );
     res.send(results.data);
+  } catch (err) {
+    res.status(404).send(err);
+  }
+});
+
+// INSERT a new topic into the DB
+router.post("/", async (req, res, next) => {
+  try {
+    await db(       
+      `insert into topic (topic_id, theme, description, image) values ('${req.body.topic_id}', '${req.body.theme}', '${req.body.description}', '${req.body.image}');`
+    );
+    res.send({ msg: "Topic inserted" });
   } catch (err) {
     res.status(404).send(err);
   }
